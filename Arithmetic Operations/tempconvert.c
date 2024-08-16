dd55cfb5e71eb820c9380fe761e59285ff843ade
Arnab-13 <arnabthe07@gmail.com>
2024-08-16 16:59:25 +0530

    Add files via upload

---
+++ Arithmetic Operations/tempconvert.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    float c,f;
+    printf("enter c\n");
+    scanf("%f",&c);
+    f=((9.0/5.0)*c)+32;
+    printf("the temp in ferenhit is %f",f);
+    return 0;
+}
