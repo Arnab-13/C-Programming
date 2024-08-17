b333aaeb4f21784423f35692a6fe84ee760d2c89
Arnab-13 <arnabthe07@gmail.com>
2024-08-17 22:43:30 +0530

    Add files via upload

---
+++ Loops Funtions/for loops/factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    int product=1;
+    printf("enter the number:");
+    scanf("%d",&n);
+    for (int i = 1; i <=n; i++)
+    {
+       product *=i;
+    }
+    printf("the factorial of %d is %d",n,product);
+    return 0;
+}
