dc3cdafcb6d26efd3a68235ee950fb8c6436d22c
Arnab-13 <arnabthe07@gmail.com>
2024-08-23 22:09:38 +0530

    Create average_funtion.c

---
+++ Funtion/average_funtion.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+float average(int a, int b, int c);
+
+float average (int a, int b, int c){
+    return (a+b+c)/3.0;
+}
+
+int main(){
+    int a=11;
+    int b=4;
+    int c=5;
+    printf("the average of the numbers is %.2f",average(a,b,c) );
+    return 0;
+}
