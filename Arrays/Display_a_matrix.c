8ec1c1b819ff42902300227ffd483c87387b2f2d
Arnab-13 <arnabthe07@gmail.com>
2025-04-07 14:34:13 +0530

    Update and rename Display_a_matrix to Display_a_matrix.c

---
+++ Arrays/Display_a_matrix.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    int matrix[3][3];
+    printf("enter the elments for matrix:");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("enter the elemen matrix[%d][%d]",i,j);
+            scanf("%d",&matrix[i][j]);
+        }
+        printf("\n");
+        
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d",matrix[i][j]);
+        }
+        printf("\n");
+        
+    }
+    
+    
+    return 0;
+}
